ca5a18471519cf130ac62f73993a30a598b824fd
Aditya Mall <contact.adityamall@gmail.com>
2023-01-23 22:50:19 +0530

    major update

--- upGov.js
+++ upGov.js
@@ -4,9 +4,12 @@ const sidebarLinkOneOne = document.getElementById("linkOne_one");
 const sidebarLinkOneTwo = document.getElementById("linkOne_two");
 const ulSidebarLinkOneOne = document.querySelector(".sidebarmenu__ulThree");
 const ulSidebarLinkOneTwo = document.querySelector(".sidebarmenu__ulFour");
-
 const sidebarButton = document.getElementById("sidebarButton");
 const sidebarmenu = document.querySelector(".sidebarmenu");
+
+const oneHeroImage = document.getElementById("one");
+const twoHeroImage = document.getElementById("two");
+const threeHeroImage = document.getElementById("three");
 
 sidebarButton.addEventListener("click", function () {
   sidebarmenu.classList.toggle("inactive");
@@ -23,3 +26,36 @@ sidebarLinkOneOne.addEventListener("click", function () {
 sidebarLinkOneTwo.addEventListener("click", function () {
   ulSidebarLinkOneTwo.classList.toggle("inactive");
 });
+
+const slides = document.querySelectorAll(".slide");
+let curSlide = 0;
+
+slides.forEach((slide, index) => {
+  slide.style.transform = `translateX(${index * 100}%)`;
+});
+
+oneHeroImage.addEventListener("click", function () {
+  slides[0].style.display = "grid";
+  slides[1].style.display = "none";
+  slides[2].style.display = "none";
+});
+
+twoHeroImage.addEventListener("click", function () {
+  slides[0].style.display = "none";
+  slides[1].style.display = "grid";
+  slides[2].style.display = "none";
+});
+
+threeHeroImage.addEventListener("click", function () {
+  slides[0].style.display = "none";
+  slides[1].style.display = "none";
+  slides[2].style.display = "grid";
+});
+
+const date = new Date();
+let getYear = date.getFullYear();
+
+const year = document.getElementById("year");
+year.innerText = `${getYear} Copyright: Developed by IDBI Intech`;
+
+console.log(getYear);
