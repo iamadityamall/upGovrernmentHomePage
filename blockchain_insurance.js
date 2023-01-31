cd94ce66fc8dc5c736b35d9fecb367c1f71c80b0
Aditya Mall <contact.adityamall@gmail.com>
2023-01-31 23:57:48 +0530

    updated

--- blockchain_insurance.js
+++ blockchain_insurance.js
@@ -38,3 +38,46 @@ questions.forEach((question) => {
     });
   });
 });
+
+
+
+
+
+
+
+const sidebarLinkOne = document.getElementById("linkOne");
+const sidebarLinkOneSubmenu = document.getElementById("submenu");
+const sidebarLinkOneOne = document.getElementById("linkOne_one");
+const sidebarLinkOneTwo = document.getElementById("linkOne_two");
+const ulSidebarLinkOneOne = document.querySelector(".sidebarmenu__ulThree");
+const ulSidebarLinkOneTwo = document.querySelector(".sidebarmenu__ulFour");
+const sidebarButton = document.getElementById("sidebarButton");
+const sidebarmenu = document.querySelector(".sidebarmenu");
+
+const oneHeroImage = document.getElementById("one");
+const twoHeroImage = document.getElementById("two");
+const threeHeroImage = document.getElementById("three");
+
+sidebarButton.addEventListener("click", function () {
+  sidebarmenu.classList.toggle("inactive");
+});
+
+sidebarLinkOne.addEventListener("click", function () {
+  sidebarLinkOneSubmenu.classList.toggle("inactive");
+});
+
+sidebarLinkOneOne.addEventListener("click", function () {
+  ulSidebarLinkOneOne.classList.toggle("inactive");
+});
+
+sidebarLinkOneTwo.addEventListener("click", function () {
+  ulSidebarLinkOneTwo.classList.toggle("inactive");
+});
+
+
+
+
+
+
+
+
